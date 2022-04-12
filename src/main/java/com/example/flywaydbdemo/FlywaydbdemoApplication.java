a67fb2398f76947ac565445333a41c28cb506b91
Ashish Sharda <ashishjsharda@gmail.com>
2022-04-12 06:40:27 -0500

    first commit

---
+++ src/main/java/com/example/flywaydbdemo/FlywaydbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.flywaydbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaydbdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywaydbdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flywaydbdemo/FlywaydbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flywaydbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywaydbdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
